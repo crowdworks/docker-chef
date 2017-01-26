a53686079b81601ced4ba843779953d2711ae2ac
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2017-01-26 14:14:09 +0900

    id_rsa作成時に使用する環境変数名が適切ではないため修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [[ -n $EC2_SSH_KEY ]]; then
-    echo -e $EC2_SSH_KEY > ~/.ssh/ec2.pem
+if [[ -n $KITCHEN_EC2_SSH_KEY ]]; then
+    echo -e $KITCHEN_EC2_SSH_KEY > ~/.ssh/ec2.pem
     chmod 400 ~/.ssh/ec2.pem
 fi
 
-if [[ -n $GITHUB_SSH_KEY ]]; then
-    echo -e $GITHUB_SSH_KEY > ~/.ssh/id_rsa
+if [[ -n $SSH_PRIVATE_KEY ]]; then
+    echo -e $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
     chmod 400 ~/.ssh/id_rsa
     ssh-keyscan github.com > ~/.ssh/known_hosts
     cat <<EOF > ~/.ssh/config
